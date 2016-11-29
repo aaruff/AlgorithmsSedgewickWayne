a1279c9dfebe3cea7b55ae0956b9f92a2abe6dea
aaruff <anwarruff@gmail.com>
2016-11-29 13:20:54 -0500

    updating hamming test and documentation

--- src/test/java/com/anwarruff/sedgewick/algorithms/week4/assignment/BoardTest.java
+++ src/test/java/com/anwarruff/sedgewick/algorithms/week4/assignment/BoardTest.java
@@ -24,10 +24,39 @@ public class BoardTest {
 
     @Test
     public void testHamming() {
+        /*
+         *   Solution
+         * +---+---+---+
+         * | 1 | 2 | 3 |
+         * +---+---+---+
+         * | 4 | 5 | 6 |
+         * +---+---+---+
+         * | 7 | 8 |   |
+         * +---+---+---+
+         *
+         *    Entry
+         * +---+---+---+
+         * | 8 | 1 | 3 |
+         * +---+---+---+
+         * | 4 |   | 2 |
+         * +---+---+---+
+         * | 7 | 6 | 5 |
+         * +---+---+---+
+         *
+         * value: (VerticalDistance, HorizontalDistance, SumHorizontalVerticalDistances)
+         * 8: (2, 1, 3)
+         * 1: (0, 1, 1)
+         * 3: (0, 0, 0)
+         * 4: (0, 0, 0)
+         * 2: (1, 1, 2)
+         * 7: (0, 0, 0)
+         * 6: (1, 1, 2)
+         * 5: (1, 1, 2)
+         * Total = 10
+         */
         int[][] initial = {{8, 1, 3}, {4, 0, 2}, {7, 6, 5}};
         Board board = new Board(initial);
-        assertEquals(3, board.hamming());
-
+        assertEquals(10, board.hamming());
     }
 
 }
