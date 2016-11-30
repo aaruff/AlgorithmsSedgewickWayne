a383de98c838217c2fb1cfffb75d0378d09da9de
aaruff <anwarruff@gmail.com>
2016-11-30 13:11:09 -0500

    implemented isGoal test using manhattan distance

--- src/main/java/com/anwarruff/sedgewick/algorithms/week4/assignment/Board.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/week4/assignment/Board.java
@@ -1,10 +1,13 @@
 package com.anwarruff.sedgewick.algorithms.week4.assignment;
 
+import java.util.Arrays;
+
 /**
  * Created by aruff on 11/28/16.
  */
 public class Board {
     private int[][] tiles;
+    private int[][] twin;
     private int hamming;
     private int manhattan;
     private boolean isGoal;
@@ -16,33 +19,38 @@ public class Board {
         }
 
         n = tiles.length;
-
         this.tiles = new int[n][n];
-        int correctTiles = 0;
+        this.twin = new int[n][n];
+
+        int k = 0;
+        int[][] swap = new int[2][2];
         for (int i = 0; i < n; ++i) {
           for (int j = 0; j < n; ++j) {
               this.tiles[i][j] = tiles[i][j];
+              this.twin[i][j] = tiles[i][j];
               int solution = i * n + (j + 1);
               if (tiles[i][j] != 0) {
+                  // store swap indices for twin board
+                  if (k < 2) {
+                      swap[k][0] = i;
+                      swap[k][1] = j;
+                      ++k;
+                  }
+
                   if (tiles[i][j] != solution) {
                       int row = (tiles[i][j] - 1) / n;
                       int col = tiles[i][j] - 1 - (n * row);
                       manhattan += Math.abs(row - i) + Math.abs(col - j);
                       ++hamming;
                   }
-                  else {
-                      ++correctTiles;
-                  }
-              }
-              else {
-                  if (i == n-1 && j == n-1) {
-                      ++correctTiles;
-                  }
               }
           }
         }
 
-        isGoal = correctTiles == n*n;
+        // swap twin tiles
+        int tmp = twin[swap[0][0]][swap[0][1]];
+        twin[swap[0][0]][swap[0][1]] = twin[swap[1][0]][swap[1][1]];
+        twin[swap[1][0]][swap[1][1]] = tmp;
     }
 
     public int manhattan() {
@@ -54,7 +62,11 @@ public class Board {
     }
 
     public boolean isGoal() {
-        return isGoal;
+        return manhattan == 0;
+    }
+
+    public int[][] twin() {
+        return twin;
     }
 
     public int dimension() {
@@ -73,4 +85,18 @@ public class Board {
         return s.toString();
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Board board = (Board) o;
+
+        return Arrays.deepEquals(tiles, board.tiles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.deepHashCode(tiles);
+    }
 }
