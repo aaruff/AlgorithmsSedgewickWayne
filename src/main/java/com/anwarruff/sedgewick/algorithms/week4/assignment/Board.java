e7ff00425357af2a21f97db7d1a5293627e1eb09
aaruff <anwarruff@gmail.com>
2016-11-30 12:10:32 -0500

    implemented calculation and caching of results for manhattan, hamming, and isGoal in the constructor

--- src/main/java/com/anwarruff/sedgewick/algorithms/week4/assignment/Board.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/week4/assignment/Board.java
@@ -5,6 +5,10 @@ package com.anwarruff.sedgewick.algorithms.week4.assignment;
  */
 public class Board {
     private int[][] tiles;
+    private int hamming;
+    private int manhattan;
+    private boolean isGoal;
+
     private int n;
     public Board(int[][] tiles) {
         if (tiles == null) {
@@ -14,42 +18,43 @@ public class Board {
         n = tiles.length;
 
         this.tiles = new int[n][n];
+        int correctTiles = 0;
         for (int i = 0; i < n; ++i) {
           for (int j = 0; j < n; ++j) {
               this.tiles[i][j] = tiles[i][j];
+              int solution = i * n + (j + 1);
+              if (tiles[i][j] != 0) {
+                  if (tiles[i][j] != solution) {
+                      int row = (tiles[i][j] - 1) / n;
+                      int col = tiles[i][j] - 1 - (n * row);
+                      manhattan += Math.abs(row - i) + Math.abs(col - j);
+                      ++hamming;
+                  }
+                  else {
+                      ++correctTiles;
+                  }
+              }
+              else {
+                  if (i == n-1 && j == n-1) {
+                      ++correctTiles;
+                  }
+              }
           }
         }
+
+        isGoal = correctTiles == n*n;
     }
 
     public int manhattan() {
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int solution = i * n + (j + 1);
-                if (tiles[i][j] != solution && tiles[i][j] != 0) {
-                    int row = (tiles[i][j]-1) / n;
-                    int col = tiles[i][j]-1 - (n * row);
-                    sum += Math.abs(row-i) + Math.abs(col - j);
-                }
-            }
-        }
-
-        return sum;
+        return manhattan;
     }
 
     public int hamming() {
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int solution = i * n + (j + 1);
-                if (tiles[i][j] != solution && tiles[i][j] != 0) {
-                    ++sum;
-                }
-            }
-        }
+        return hamming;
+    }
 
-        return sum;
-
+    public boolean isGoal() {
+        return isGoal;
     }
 
     public int dimension() {
