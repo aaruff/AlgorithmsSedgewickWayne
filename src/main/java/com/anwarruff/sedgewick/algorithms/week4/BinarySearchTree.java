637a9e7adc049c9935592e4b6de6817ef4399a56
aaruff <anwarruff@gmail.com>
2016-12-13 14:07:23 -0500

    implemented put, and added test

---
+++ src/main/java/com/anwarruff/sedgewick/algorithms/week4/BinarySearchTree.java
@@ -0,0 +1,85 @@
+package com.anwarruff.sedgewick.algorithms.week4;
+
+import java.util.Iterator;
+
+/**
+ * Created by aruff on 12/12/16.
+ */
+public class BinarySearchTree<Key extends Comparable<Key>, Value> {
+    private Node root;
+
+    public void put(Key key, Value value) {
+        root = insertNode(root, key, value);
+    }
+
+    private Node insertNode(Node node, Key key, Value value) {
+        // If the child is null, instantiate a new node with the key and value and pass it back to the parent
+        if (node == null) {
+            return new Node(key, value);
+        }
+
+        int compareResult = key.compareTo(node.key);
+        if (compareResult < 0) {
+            node.left = insertNode(node.left, key, value);
+        }
+        else if (compareResult > 0) {
+            node.right = insertNode(node.right, key, value);
+        }
+        else {
+            node.value = value;
+        }
+
+        return node;
+    }
+
+
+    public Value get(Key key) {
+        Node node = getNode(root, key);
+        if (node == null) {
+            return null;
+        }
+
+        return node.value;
+    }
+
+    private Node getNode(Node node, Key key) {
+        if (node == null) {
+            return null;
+        }
+
+        int compareResult = key.compareTo(node.key);
+        if (compareResult < 0) {
+            return getNode(node.left, key);
+        }
+        else if (compareResult > 1) {
+            return getNode(node.right, key);
+        }
+        else {
+            return node;
+        }
+    }
+
+    public void delete(Key key) {
+
+    }
+
+    public Iterable<Key> iterator() {
+        return new Iterable<Key>() {
+            @Override
+            public Iterator<Key> iterator() {
+                return null;
+            }
+        };
+    }
+
+    public class Node {
+        private Key key;
+        private Value value;
+        private Node left, right;
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
---
+++ src/test/java/com/anwarruff/sedgewick/algorithms/week4/BinarySearchTreeTest.java
@@ -0,0 +1,25 @@
+package com.anwarruff.sedgewick.algorithms.week4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aruff on 12/13/16.
+ */
+public class BinarySearchTreeTest {
+    @Test
+    public void testBSTPut() {
+        String[] str = {"S", "E", "A", "R", "C", "H", "E", "X", "A", "M", "P", "L", "E"};
+        Integer[] exp = {0, 12, 8, 3, 4, 5, 12, 7, 8, 9, 10, 11, 12};
+        BinarySearchTree<String, Integer> bst = new BinarySearchTree<>();
+        for (Integer i = 0; i < str.length; i++) {
+            bst.put(str[i], i);
+        }
+
+        for (Integer i = 0; i < str.length; i++) {
+            assertEquals(exp[i], bst.get(str[i]));
+        }
+    }
+
+}
