1055393693cb67d9d78a4b8350646cca9b370230
aaruff <anwarruff@gmail.com>
2016-12-22 15:13:09 -0500

    implemented Bag and unit tests

---
+++ src/test/java/com/anwarruff/sedgewick/algorithms/chapter1/BagTest.java
@@ -0,0 +1,33 @@
+package com.anwarruff.sedgewick.algorithms.chapter1;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by aruff on 12/22/16.
+ */
+public class BagTest {
+    @Test
+    public void testBagIterator() throws Exception {
+        ArrayList<String> entries = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G"));
+        Bag<String> b = new Bag<>();
+        for (String s : entries) {
+            b.add(s);
+        }
+
+        assertEquals(entries.size(), b.size());
+
+        for (String s : b) {
+            assertTrue(entries.contains(s));
+            entries.remove(s);
+        }
+
+        assertEquals(0, entries.size());
+    }
+
+}
