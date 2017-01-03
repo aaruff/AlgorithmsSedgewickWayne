3d4fdfa7309f10b306d7554d303c4670c716785d
aaruff <anwarruff@gmail.com>
2017-01-03 06:35:07 -0500

    implemented graph file loader, test, and copied graph files to resource dir

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/GraphFactory.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/GraphFactory.java
@@ -1,7 +1,39 @@
 package com.anwarruff.sedgewick.algorithms.course.part2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
- * Created by aruff on 1/3/17.
+ * Produces a Graph given a correctly formatted resource file.
+ * For formatting see: http://algs4.cs.princeton.edu/41graph/tinyCG.txt
  */
 public class GraphFactory {
+    private Graph graph;
+    public GraphFactory(String fileName) {
+        File file = new File(getClass().getClassLoader().getResource("part2/week1/" + fileName).getFile());
+        try {
+            Scanner scanner = new Scanner(file);
+
+            int vertices = Integer.valueOf(scanner.nextLine());
+            graph = new Graph(vertices);
+
+            int edges = Integer.valueOf(scanner.nextLine());
+            int i = 0;
+            while (scanner.hasNextLine() && i < edges) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(" ");
+                int v = Integer.valueOf(parts[0]);
+                int w = Integer.valueOf(parts[1]);
+                graph.addEdge(v, w);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public Graph getGraph() {
+        return graph;
+    }
 }
---
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/TinyGPrinter.java
@@ -0,0 +1,11 @@
+package com.anwarruff.sedgewick.algorithms.course.part2;
+
+/**
+ * Created by aruff on 1/3/17.
+ */
+public class TinyGPrinter {
+    public static void main(String[] args) {
+        GraphFactory graphFactory = new GraphFactory("tinyCG.txt");
+        System.out.println(graphFactory.getGraph());
+    }
+}
