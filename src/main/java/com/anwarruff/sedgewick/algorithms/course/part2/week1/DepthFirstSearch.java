d5959d9a5a9b654bc4d01deabed5ca3d48e1bd0b
aaruff <anwarruff@gmail.com>
2017-01-05 15:07:29 -0500

    implemented DFS and Test

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/DepthFirstSearch.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/DepthFirstSearch.java
@@ -4,4 +4,29 @@ package com.anwarruff.sedgewick.algorithms.course.part2.week1;
  * Created by aruff on 1/5/17.
  */
 public class DepthFirstSearch {
+    private boolean[] marked;
+    private int count;
+
+    public DepthFirstSearch(Graph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        count++;
+        for (int w : G.adj(v)) {
+            if (! marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean marked(int w) {
+        return marked[w];
+    }
+
+    public int count() {
+        return count;
+    }
 }
--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/TestSearch.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/TestSearch.java
@@ -4,4 +4,26 @@ package com.anwarruff.sedgewick.algorithms.course.part2.week1;
  * Created by aruff on 1/5/17.
  */
 public class TestSearch {
+    public static void test(Graph G, int s) {
+        DepthFirstSearch search = new DepthFirstSearch(G, s);
+
+        for (int v = 0; v < G.V(); v++) {
+            if (search.marked(v)) {
+                System.out.print(v + " ");
+            }
+        }
+        System.out.println();
+
+        if (search.count() != G.V()) {
+            System.out.println("NOT ");
+        }
+        System.out.println("connected");
+    }
+
+    public static void main(String[] args) {
+        GraphFactory graphFactory = new GraphFactory("tinyG.txt");
+        Graph G = graphFactory.getGraph();
+        System.out.println(G);
+        TestSearch.test(G, 3);
+    }
 }
