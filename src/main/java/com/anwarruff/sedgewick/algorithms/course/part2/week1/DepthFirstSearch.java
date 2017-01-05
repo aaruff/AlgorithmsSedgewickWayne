6aa3762167c496d9b33d6b9f7d9085b7d3f4cd40
aaruff <anwarruff@gmail.com>
2017-01-04 21:36:13 -0500

    updated DFS

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/DepthFirstSearch.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/DepthFirstSearch.java
@@ -1,34 +1,47 @@
 package com.anwarruff.sedgewick.algorithms.course.part2.week1;
 
+import com.anwarruff.sedgewick.algorithms.textbook.chapter1.section3.Stack;
+
 /**
  * Created by aruff on 1/2/17.
  */
 public class DepthFirstSearch {
     private boolean marked[];
-    private int count;
+    private int[] edgeTo;
+    private int s;
 
     public DepthFirstSearch(Graph G, int s) {
         marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
         dfs(G, s);
     }
 
     private void dfs(Graph G, int v) {
         marked[v] = true;
-        count++;
         for (int w: G.adj(v)) {
             if ( ! marked[w]) {
+                edgeTo[w] = v; // mark record of going to w from v
                 dfs(G, w);
             }
         }
     }
 
-    public boolean marked(int w) {
-        return marked[w];
+    public boolean hasPathTo(int v) {
+        return marked[v];
     }
 
-    public int count() {
-        return count;
+    public Iterable<Integer> pathTo(int v) {
+        if (! hasPathTo(v)) {
+            return null;
+        }
+
+        Stack<Integer> path = new Stack<>();
+        for (int x = v; x != s; x = edgeTo[x]) {
+            path.push(x);
+        }
+
+        path.push(s);
+        return path;
     }
-
-
 }
