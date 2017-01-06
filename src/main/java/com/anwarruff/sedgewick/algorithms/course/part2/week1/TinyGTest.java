79f233a8b27dab6b188b9d7a3f08dac18724fe16
aaruff <anwarruff@gmail.com>
2017-01-05 19:24:24 -0500

    renamed test file, and added print statemnt for test path

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/TinyGTest.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week1/TinyGTest.java
@@ -3,9 +3,12 @@ package com.anwarruff.sedgewick.algorithms.course.part2.week1;
 /**
  * Created by aruff on 1/3/17.
  */
-public class TinyGPrinter {
+public class TinyGTest {
     public static void main(String[] args) {
-        GraphFactory graphFactory = new GraphFactory("tinyCG.txt");
-        System.out.println(graphFactory.getGraph());
+        GraphFactory graphFactory = new GraphFactory("tinyG.txt");
+        Graph G = graphFactory.getGraph();
+        DepthFirstPaths depthFirstPaths = new DepthFirstPaths(G, 0);
+
+        depthFirstPaths.printPath(3);
     }
 }
