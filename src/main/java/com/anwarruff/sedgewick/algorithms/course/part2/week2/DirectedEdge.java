4ec83c6d9c705c7a1f6e2caf01fe4fed2c17f414
aaruff <anwarruff@gmail.com>
2017-02-01 13:26:11 -0500

    implemented directed edge

---
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week2/DirectedEdge.java
@@ -0,0 +1,32 @@
+package com.anwarruff.sedgewick.algorithms.course.part2.week2;
+
+/**
+ * Created by aruff on 2/1/17.
+ */
+public class DirectedEdge {
+    private final int v;
+    private final int w;
+    private final double weight;
+
+    public DirectedEdge(int v, int w, double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+
+    public String toString() {
+        return String.format("%d->%d %.2f", v, w, weight);
+    }
+}
