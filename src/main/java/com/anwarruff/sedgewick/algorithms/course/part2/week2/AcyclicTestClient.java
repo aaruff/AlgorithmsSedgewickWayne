e002b0d278da6a1791215c9f394bd0c56c2e1307
aaruff <anwarruff@gmail.com>
2017-02-08 17:40:11 -0500

    completed implementation of the acyclic quiz checker

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week2/AcyclicTestClient.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week2/AcyclicTestClient.java
@@ -1,6 +1,10 @@
 package com.anwarruff.sedgewick.algorithms.course.part2.week2;
 
+import edu.princeton.cs.algs4.DirectedEdge;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by aruff on 2/6/17.
@@ -11,14 +15,30 @@ public class AcyclicTestClient {
         EdgeWeightedGraphFactory factory = new EdgeWeightedGraphFactory("Quiz2.txt", vertexType);
         VertexEncoder encoder = factory.getEncoding();
 
-        AcyclicShortestPath dsp = new AcyclicShortestPath(factory.getGraph(), encoder.getEncoding("E"));
-        ArrayList<DirectedEdge<String>> list = new ArrayList<>();
-        for (edu.princeton.cs.algs4.DirectedEdge e : dsp.getPaths()) {
-            String v = encoder.getEncoding(e.from());
-            String w = encoder.getEncoding(e.to());
-            double weight = e.weight();
-            System.out.println(v + "->" + w + " " + String.format("%5.2f", weight));
+        ArrayList<Integer> topologicalOrder = new ArrayList<>();
+        for (String s : Arrays.asList("C", "H", "D", "G", "F")) {
+            topologicalOrder.add(encoder.getEncoding(s));
         }
 
+        int source = encoder.getEncoding("C");
+        AcyclicShortestPath asp = new AcyclicShortestPath(topologicalOrder, factory.getGraph(), source);
+
+        ArrayList<ComparableDirectedEdge> edges = new ArrayList<>();
+        for (DirectedEdge e : asp.getPaths()) {
+            String from = encoder.getEncoding(e.from());
+            String to = encoder.getEncoding(e.to());
+            double weight = e.weight();
+            edges.add(new ComparableDirectedEdge(from, to, weight));
+        }
+
+        Collections.sort(edges);
+
+        for (ComparableDirectedEdge e : edges) {
+            System.out.println(e.from() + "->" + e.to() + " " + String.format("%5.2f", e.weight()));
+        }
+
+        for (ComparableDirectedEdge e : edges) {
+            System.out.print(String.format("%d ", (long) e.weight()));
+        }
     }
 }
