6d012352ad1a5712bb8b58599bfec179f0f44841
aaruff <anwarruff@gmail.com>
2017-02-12 19:03:37 -0500

    completed assignment 2

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week2/assignment/SeamCarver.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week2/assignment/SeamCarver.java
@@ -9,12 +9,18 @@ import java.awt.*;
  */
 public class SeamCarver {
     private Picture picture;
+    private int rows;
+    private int columns;
 
     public SeamCarver(Picture picture) {
         if (picture == null)
             throw new NullPointerException();
 
         this.picture = new Picture(picture);
+        rows = picture.height();
+        columns = picture.width();
+
+
     }
 
     public Picture picture() {
@@ -40,11 +46,11 @@ public class SeamCarver {
     }
 
     public double energy(int col, int row) {
-        if (col < 0 || col > picture.width()-1 || row < 0 || row > picture.height()-1)
-            throw new IndexOutOfBoundsException();
+        if (col < 0 || col > columns-1 || row < 0 || row > rows-1)
+            throw new IndexOutOfBoundsException("column = " + col + ", row = " + row);
 
         final int BorderPixelEnergy = 1000;
-        if (col == 0 || col == picture.width()-1 || row == 0 || row == picture.height()-1)
+        if (col == 0 || col == columns-1 || row == 0 || row == rows-1)
             return BorderPixelEnergy;
 
         int colGradient = squaredGradient(col + 1, row, col - 1, row);
@@ -52,59 +58,222 @@ public class SeamCarver {
         return Math.sqrt(colGradient + rowGradient);
     }
 
-    // TODO
     public int[] findHorizontalSeam() {
-        int[] seam = new int[picture.width()];
-        return seam;
+        if (columns == 1) {
+            return new int[] {0};
+        }
+
+        int[][] from = new int[rows][columns];
+        double[][] weight = new double[rows][columns];
+
+        for (int col = 0; col < columns; col++) {
+            for (int row = 0; row < rows; row++) {
+                if (col == 0) {
+                    weight[row][col] = energy(col, row);
+                    from[row][col] = row;
+                }
+                else {
+                    relaxHorizontalEdges(row, col, from, weight);
+                }
+            }
+        }
+
+        int[] path = new int[columns];
+        int minRow = findMinRow(weight, columns - 1);
+        for (int col = columns - 1; col >= 0; col--) {
+            path[col] = minRow;
+            minRow = from[minRow][col];
+        }
+
+        return path;
     }
 
-    // TODO
     public int[] findVerticalSeam() {
-        int[] seam = new int[picture.height()];
-        return seam;
+        if (rows == 1) {
+            return new int[] {0};
+        }
+
+        int[][] from = new int[rows][columns];
+        double[][] weight = new double[rows][columns];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                if (row == 0) {
+                    weight[row][col] = energy(col, row);
+                    from[row][col] = col;
+                }
+                else {
+                    relaxVerticalEdges(row, col, from, weight);
+                }
+            }
+        }
+
+        int[] path = new int[rows];
+        int minColumn = findMinColumn(weight, rows - 1);
+        for (int row = rows - 1; row >= 0; row--) {
+            path[row] = minColumn;
+            minColumn = from[row][minColumn];
+        }
+
+        return path;
     }
 
-    // TODO
+    private void relaxVerticalEdges(int row, int col, int[][] from, double[][] weight) {
+        int parentRow = row - 1;
+        double parentWeight = weight[parentRow][col];
+
+        double middleChildWeight = energy(col, row) + parentWeight;
+        if (weight[row][col] == 0 || middleChildWeight < weight[row][col]) {
+            weight[row][col] = middleChildWeight;
+            from[row][col] = col;
+        }
+
+        if (col > 0 && col <= columns - 1) {
+            double leftChildWeight = energy(col - 1, row) + parentWeight;
+            if (weight[row][col - 1] == 0 || leftChildWeight < weight[row][col - 1]) {
+                weight[row][col - 1] = leftChildWeight;
+                from[row][col - 1] = col;
+            }
+        }
+
+        if (col >= 0 && col < columns - 1) {
+            double rightChildWeight = energy(col + 1, row) + parentWeight;
+            if (weight[row][col + 1] == 0 || rightChildWeight < weight[row][col + 1]) {
+                weight[row][col + 1] = rightChildWeight;
+                from[row][col + 1] = col;
+            }
+        }
+    }
+
+    private void relaxHorizontalEdges(int row, int col, int[][] from, double[][] weight) {
+        int parentColumn = col - 1;
+        double parentWeight = weight[row][parentColumn];
+
+        double middleChildWeight = energy(col, row) + parentWeight;
+        if (weight[row][col] == 0 || middleChildWeight < weight[row][col]) {
+            weight[row][col] = middleChildWeight;
+            from[row][col] = row;
+        }
+
+        if (row > 0 && row <= rows - 1) {
+            double topChildWeight = energy(col, row - 1) + parentWeight;
+            if (weight[row - 1][col] == 0 || topChildWeight < weight[row - 1][col]) {
+                weight[row - 1][col] = topChildWeight;
+                from[row - 1][col] = row;
+            }
+        }
+
+        if (row >= 0 && row < rows - 1) {
+            double bottomChildWeight = energy(col, row + 1) + parentWeight;
+            if (weight[row + 1][col] == 0 || bottomChildWeight < weight[row + 1][col]) {
+                weight[row + 1][col] = bottomChildWeight;
+                from[row + 1][col] = row;
+            }
+        }
+    }
+
+    private int findMinColumn(double[][] cost, int index) {
+        int minIndex = 0;
+        for (int i = 1; i < columns; i++) {
+            if (cost[index][minIndex] > cost[index][i]) {
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
+    private int findMinRow(double[][] cost, int index) {
+        int minIndex = 0;
+        for (int i = 1; i < rows; i++) {
+            if (cost[minIndex][index] > cost[i][index]) {
+                minIndex = i;
+            }
+        }
+
+        return minIndex;
+    }
+
     private boolean isValidRowSeam(int[] seam) {
         if (seam == null)
             throw new NullPointerException();
-        if (seam.length != picture.width() || picture.height() <= 1)
+        if (seam.length != columns)
             throw new IllegalArgumentException();
 
-        // TODO
-        // also throw Illegal argument exception if
-        // entry is outside it's prescribed range or,
-        // two adjacent entries differ by more than one
-        return false;
+        for (int i = 0; i < seam.length; i++) {
+           if (seam[i] < 0 || seam[i] >= rows) {
+               throw new IllegalArgumentException();
+           }
+           if (i > 0) {
+               int diff = seam[i] - seam[i-1];
+               if (diff != 1 && diff != -1 && diff != 0) {
+                   throw new IllegalArgumentException();
+               }
+           }
+        }
+        return true;
     }
 
-    // TODO
+
     private boolean isValidColumnSeam(int[] seam) {
         if (seam == null)
             throw new NullPointerException();
-        if (seam.length != picture.width() || picture.width() <= 1)
+        if (seam.length != rows)
             throw new IllegalArgumentException();
 
-        // TODO
-        // also throw Illegal argument exception if
-        // entry is outside it's prescribed range or,
-        // two adjacent entries differ by more than one
-        return false;
+        for (int i = 0; i < seam.length; i++) {
+            if (seam[i] < 0 || seam[i] >= columns) {
+                throw new IllegalArgumentException();
+            }
+            if (i > 0) {
+                int diff = seam[i] - seam[i-1];
+                if (diff != 1 && diff != -1 && diff != 0) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
+        return true;
     }
 
 
-    // TODO
     public void removeHorizontalSeam(int[] seam) {
         if ( ! isValidRowSeam(seam))
             return;
 
+        Picture picture = new Picture(columns, rows - 1);
+        for (int col = 0; col < columns; col++) {
+            int skip = seam[col];
+            for (int row = 0, prow = 0; row < rows; row++) {
+                if (row == skip)
+                    continue;
+                picture.set(col, prow, this.picture.get(col, row));
+                ++prow;
+            }
+        }
+
+        this.picture = picture;
+        rows = picture.height();
+        columns = picture.width();
     }
 
-    // TODO
     public void removeVerticalSeam(int[] seam) {
         if ( ! isValidColumnSeam(seam))
             return;
 
+        Picture picture = new Picture(columns - 1, rows);
+        for (int row = 0; row < rows; row++) {
+            int skip = seam[row];
+            for (int col = 0, pcol = 0; col < columns; col++) {
+                if (col == skip)
+                    continue;
+                picture.set(pcol, row, this.picture.get(col, row));
+                ++pcol;
+            }
+        }
+
+        this.picture = picture;
+        rows = picture.height();
+        columns = picture.width();
     }
 
 }
