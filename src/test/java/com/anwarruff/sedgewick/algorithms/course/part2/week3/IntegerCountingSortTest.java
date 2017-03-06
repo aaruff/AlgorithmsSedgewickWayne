13d668e574a2fea82140973d1e6e09d0d6f76e4b
aaruff <anwarruff@gmail.com>
2017-03-06 16:05:16 -0500

    Implemented test for counting sort

--- src/test/java/com/anwarruff/sedgewick/algorithms/course/part2/week3/IntegerCountingSortTest.java
+++ src/test/java/com/anwarruff/sedgewick/algorithms/course/part2/week3/IntegerCountingSortTest.java
@@ -8,12 +8,12 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by aruff on 3/6/17.
  */
-public class CountingSortTest {
+public class IntegerCountingSortTest {
     @Test
     public void testAlgorithm() throws Exception {
         int[] input = new int[]{1, 2, 3, 4, 2, 0, 1, 3, 4};
         int[] solution = new int[]{0, 1, 1, 2, 2, 3, 3, 4, 4};
-        int[] sorted = CountingSort.sort(input);
+        int[] sorted = IntegerCountingSort.sort(input);
         for (int i = 0; i < solution.length; i++) {
             assertEquals(solution[i], sorted[i]);
         }
