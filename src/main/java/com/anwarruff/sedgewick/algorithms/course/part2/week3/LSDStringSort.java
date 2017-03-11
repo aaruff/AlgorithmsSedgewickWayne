76fad8913ea9a5bddbdff6e9ff14e8f4fea27faf
aaruff <anwarruff@gmail.com>
2017-03-10 19:26:09 -0500

    fixed index out of bounds

--- src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week3/LSDStringSort.java
+++ src/main/java/com/anwarruff/sedgewick/algorithms/course/part2/week3/LSDStringSort.java
@@ -4,27 +4,29 @@ package com.anwarruff.sedgewick.algorithms.course.part2.week3;
  * Created by aruff on 3/9/17.
  */
 public class LSDStringSort {
-    public static void sort(String[] strings, int fixedLength) {
+    public static void sort(String[] strings, int stringLength) {
         final int RADIX = 256;
-        String[] sorted = new String[strings.length];
+        final int NUM_STRINGS = strings.length;
 
-        for (int column = fixedLength-1; column >= 0; --column) {
-            int[] count = new int[RADIX - 1];
+        String[] sortedStrings = new String[NUM_STRINGS];
+
+        for (int position = stringLength-1; position >= 0; --position) {
+            int[] count = new int[RADIX + 1]; // zero'th index left blank
             // Count the frequencies
-            for (int i = 0; i < strings.length; i++) {
-                count[strings[i].charAt(column) + 1]++;
+            for (int i = 0; i < NUM_STRINGS; i++) {
+                count[strings[i].charAt(position) + 1]++;
             }
             // Count the characters before each char
             for (int r = 0; r < RADIX; r++) {
                 count[r + 1] += count[r];
             }
             // Sort
-            for (int i = 0; i < strings.length; i++) {
-                sorted[ count[strings[i].charAt(column)]++] = strings[i];
+            for (int i = 0; i < NUM_STRINGS; i++) {
+                sortedStrings[ count[strings[i].charAt(position)]++] = strings[i];
             }
             // Put the sorted result back into the input array
-            for (int i = 0; i < strings.length; i++) {
-                strings[i] = sorted[i];
+            for (int i = 0; i < NUM_STRINGS; i++) {
+                strings[i] = sortedStrings[i];
             }
         }
     }
